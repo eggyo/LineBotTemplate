446f42910af1e3bcc8d1cf3270b6bc760a634327
eggyo <eggyoyo4859@gmail.com>
2016-07-08 14:27:40 +0700

    v.3.2

--- main.go
+++ main.go
@@ -13,49 +13,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
-	"net/url"
-  	"io/ioutil"
+
 	"github.com/line/line-bot-sdk-go/linebot"
-        "encoding/json"
 )
 
 var bot *linebot.Client
 
 type GeoContent struct {
 	LatLong string `json:"latLon"`
-	Utm string `json:"utm"`
-        Mgrs string `json:"mgrs"`
+	Utm     string `json:"utm"`
+	Mgrs    string `json:"mgrs"`
 }
 
 type ResultGeoLoc struct {
 	Results GeoContent `json:"result"`
 }
+
 func getGeoLoc(body []byte) (*ResultGeoLoc, error) {
-    var s = new(ResultGeoLoc)
-    err := json.Unmarshal(body, &s)
-    if(err != nil){
-        fmt.Println("whoops:", err)
-    }
-    return s, err
+	var s = new(ResultGeoLoc)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	return s, err
 }
 
 func main() {
 	// fixie
 	fixieUrl, err := url.Parse(os.Getenv("FIXIE_URL"))
-  	customClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(fixieUrl)}}
-  	resp, err := customClient.Get("http://welcome.usefixie.com")
-  	if (err != nil) {
-    		println(err.Error())
-    		return
-  	}
-  	defer resp.Body.Close()
-  	body, err := ioutil.ReadAll(resp.Body)
-  	println(string(body))
+	customClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(fixieUrl)}}
+	resp, err := customClient.Get("http://welcome.usefixie.com")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	println(string(body))
 
 	// end fixie
 
@@ -94,44 +96,43 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println("RECEIVE Msg:", content.IsMessage, " OP:", content.IsOperation, " type:", content.ContentType, " from:", content.From, "to:", content.To, " ID:", content.ID)
 		}
 		/*
-		if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
-			text, err := content.TextContent()
-			_, err = bot.SendText([]string{content.From}, "Bot received : "+text.Text)
+			if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
+				text, err := content.TextContent()
+				_, err = bot.SendText([]string{content.From}, "Bot received : "+text.Text)
 
-			if err != nil {
-				log.Println(err)
-			}
-		}*/
+				if err != nil {
+					log.Println(err)
+				}
+			}*/
 		if content != nil && content.ContentType == linebot.ContentTypeLocation {
 			loc, err := content.LocationContent()
 
 			// add eggyo geo test
 			resp, err := http.Get("http://eggyo-geo-node.herokuapp.com/geo/" + FloatToString(loc.Latitude) + "/" + FloatToString(loc.Longitude))
-			if (err != nil) {
-    				println(err.Error())
-    				return
-  			}
-  			defer resp.Body.Close()
+			if err != nil {
+				println(err.Error())
+				return
+			}
+			defer resp.Body.Close()
 			body, err := ioutil.ReadAll(resp.Body)
-                       log.Println(string(body))
+			log.Println(string(body))
 
-			var elev = callGoogleElev(loc.Latitude,loc.Longitude)
-                       geo, err := getGeoLoc([]byte(body))
-			_, err = bot.SendText([]string{content.From}, "LatLong :" + geo.Results.LatLong)
-			_, err = bot.SendText([]string{content.From}, "Utm :" + geo.Results.Utm)
-			_, err = bot.SendText([]string{content.From}, "Mgrs :" + geo.Results.Mgrs)
-			_, err = bot.SendText([]string{content.From}, "Altitude :" + elev)
+			var elev = callGoogleElev(loc.Latitude, loc.Longitude)
+			geo, err := getGeoLoc([]byte(body))
+			_, err = bot.SendText([]string{content.From}, "LatLong :"+geo.Results.LatLong)
+			_, err = bot.SendText([]string{content.From}, "Utm :"+geo.Results.Utm)
+			_, err = bot.SendText([]string{content.From}, "Mgrs :"+geo.Results.Mgrs)
+			_, err = bot.SendText([]string{content.From}, "Altitude :"+elev)
 
-
-                        if err != nil {
+			if err != nil {
 				log.Println(err)
 			}
 		}
 	}
 }
 func FloatToString(input_num float64) string {
-    // to convert a float number to a string
-    return strconv.FormatFloat(input_num, 'f', 6, 64)
+	// to convert a float number to a string
+	return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 
 //eggyo
--- process.go
+++ process.go
@@ -1,36 +1,44 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-func callGoogleElev(lat float64,lon float64) string {
+func callGoogleElev(lat float64, lon float64) string {
 	resp, err := http.Get("https://maps.googleapis.com/maps/api/elevation/json?locations=" + FloatToString(lat) + "," + FloatToString(lon) + "&key=AIzaSyAn9cWoce9zGEfGjDzMg6r_uTTUw3WoMOg")
-	if (err != nil) {
-    		println(err.Error())
-    		return string(err.Error())
-  	}
-  	defer resp.Body.Close()
+	if err != nil {
+		println(err.Error())
+		return string(err.Error())
+	}
+	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-       elev, err := getElev([]byte(body))
+	elev, err := getElev([]byte(body))
 
 	return FloatToString(elev.Results[0].Elev)
 }
+
 type ELEV struct {
 	Elev float64 `json:"elevation"`
-	Res float64 `json:"resolution"`
-	Loc struct {
-		Lat float64 `json:"lat"`
+	Res  float64 `json:"resolution"`
+	Loc  struct {
+		Lat  float64 `json:"lat"`
 		Long float64 `json:"lng"`
 	} `json:"location"`
 }
 
 type ResultElev struct {
 	Results []ELEV `json:"results"`
-	Status string  `json:"status"`
+	Status  string `json:"status"`
 }
+
 func getElev(body []byte) (*ResultElev, error) {
-    var s = new(ResultElev)
-    err := json.Unmarshal(body, &s)
-    if(err != nil){
-        fmt.Println("whoops:", err)
-    }
-    return s, err
+	var s = new(ResultElev)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	return s, err
 }
