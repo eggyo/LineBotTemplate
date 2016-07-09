eb04adc8b612ac731f985e944d500576f25ddf06
eggyo <eggyoyo4859@gmail.com>
2016-07-09 15:13:29 +0700

    v.3.8.1

--- textCheck.go
+++ textCheck.go
@@ -9,7 +9,7 @@ func messageCheck(msg string) string {
 		msg = strings.Trim(msg, "#ask ")
 		msg = strings.Replace(msg, " #ans ", ":", 1)
 		var msgArray = strings.Split(msg, ":")
-		if checkNewMessage(msg) == true {
+		if checkNewMessage(msgArray[0]) == true {
 			addNewMessageFromUser(msgArray[0], msgArray[1])
 		} else {
 			addReplyMessageFromUser(msgArray[0], msgArray[1])
