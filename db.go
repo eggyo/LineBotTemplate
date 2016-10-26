4fae2c692c740b8d0577bbb89395c334fc42f7f3
eggyo <eggyoyo4859@gmail.com>
2016-10-26 23:49:41 +0700

    v.5.1.1

---
+++ db.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type ObjectId struct {
+	ObjId string `json:"$oid"`
+}
+type USER struct {
+	LineID string   `json:"lineID"`
+	ID     ObjectId `json:"_id"`
+}
+type MESSAGE struct {
+	Msg      string   `json:"msg"`
+	ReplyMsg []string `json:"replyMsg"`
+	ID       ObjectId `json:"_id"`
+}
+type ResultsMESSAGE struct {
+	ContentMsg []MESSAGE
+}
+
+var userDb_url = "https://api.mlab.com/api/1/databases/heroku_h1g317z7/collections/_User?apiKey=1S26M0Ti2t7gKunYRJiGNg8aeIMXnptN"
+var msgDb_url = "https://api.mlab.com/api/1/databases/heroku_h1g317z7/collections/Message?apiKey=1S26M0Ti2t7gKunYRJiGNg8aeIMXnptN"
+
+func getAllUser() {
+
+	resp, err := http.Get(userDb_url)
+	if err != nil {
+		println(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("mLab User", string(body))
+
+}
+func addNewUser(ID string) {
+	var sendingMsg = `{"lineID":"` + ID + `"}`
+	var jsonStr = []byte(sendingMsg)
+	req, err := http.NewRequest("POST", userDb_url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
+
+func addNewMessageFromUser(msg string, replyMsg string) {
+	var sendingMsg = `{"msg":"` + msg + `","replyMsg":["` + replyMsg + `"]}`
+	log.Println(sendingMsg)
+	var jsonStr = []byte(sendingMsg)
+	req, err := http.NewRequest("POST", msgDb_url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("obj :", string(body))
+
+	msgObj, err := messageGet([]byte(body))
+	log.Println("objId msg :", msgObj.ID.ObjId)
+}
+func addReplyMessageFromUser(msg string, replyMsg string) {
+	var sendingMsg = `{ $push: {"replyMsg":"` + replyMsg + `"}}`
+	log.Println(sendingMsg)
+	var jsonStr = []byte(sendingMsg)
+	var q = `&q={"msg":"` + msg + `"}`
+	req, err := http.NewRequest("PUT", msgDb_url+q, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("obj :", string(body))
+
+	msgObj, err := messageGet([]byte(body))
+	log.Println("objId msg :", msgObj.ID.ObjId)
+}
+func getReplyMessageFromUser(msg string) string {
+	var q = `&q={"msg":"` + msg + `"}`
+	resp, err := http.Get(msgDb_url + q)
+	log.Println("Query :", msgDb_url+q)
+
+	if err != nil {
+		println(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("reply body:", string(body))
+
+	msgObjs := messageArrayGet([]byte(body))
+	log.Println("reply :", msgObjs)
+	if len(msgObjs) == 0 {
+		return "ข้าไม่เข้าใจที่เจ้าพูด แต่ถ้าอยากสอนข้า ให้ทำตามนี้\n\nพิมพ์ #ask ข้อความ #ans ข้อความที่จะให้ตอบ\n\nเช่น\n#ask หวัดดี #ans จ้า"
+	} else {
+		return msgObjs[0].ReplyMsg[0]
+	}
+}
+func checkNewMessage(msg string) bool {
+	var q = `&q={"msg":"` + msg + `"}`
+	resp, err := http.Get(msgDb_url + q)
+	log.Println("Query :", msgDb_url+q)
+
+	if err != nil {
+		println(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("check msg reply body:", string(body))
+	msgObjs := messageArrayGet([]byte(body))
+	log.Println("check msg reply :", msgObjs)
+	if len(msgObjs) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func messageGet(body []byte) (*MESSAGE, error) {
+	var s = new(MESSAGE)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	return s, err
+}
+func messageArrayGet(body []byte) []MESSAGE {
+	var m []MESSAGE
+
+	err := json.Unmarshal(body, &m)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	return m
+}
---
+++ main.go
@@ -0,0 +1,210 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+/*
+<<<<<<< HEAD
+=======
+>>>>>>> 09a61a620751c49e8b67d3244f5280b4b309e1ae
+=======
+>>>>>>> 66cf34abf6b2ed7caa5c018b73c50380be01e401
+var bot *linebot.Client
+var eggyoID = "ufa92a3a52f197e19bfddeb5ca0595e93"
+var logNof = "open"
+
+type GeoContent struct {
+	LatLong string `json:"latLon"`
+	Utm     string `json:"utm"`
+	Mgrs    string `json:"mgrs"`
+}
+
+type ResultGeoLoc struct {
+	Results GeoContent `json:"result"`
+}
+
+func getGeoLoc(body []byte) (*ResultGeoLoc, error) {
+	var s = new(ResultGeoLoc)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	return s, err
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 66cf34abf6b2ed7caa5c018b73c50380be01e401
+}*/
+
+func main() {
+
+	bot, err := linebot.New(
+		os.Getenv("ChannelSecret"),
+		os.Getenv("MID"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+<<<<<<< HEAD
+=======
+}
+
+func main() {
+	getAllUser()
+	// line bot
+	strID := os.Getenv("ChannelID")
+	numID, err := strconv.ParseInt(strID, 10, 64)
+	if err != nil {
+		log.Fatal("Wrong environment setting about ChannelID")
+	}
+
+	bot, err = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/callback", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
+	//test
+
+}
+>>>>>>> 09a61a620751c49e8b67d3244f5280b4b309e1ae
+=======
+>>>>>>> 66cf34abf6b2ed7caa5c018b73c50380be01e401
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+
+	received, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, result := range received.Results {
+		content := result.Content()
+		log.Println("-->", content)
+
+		//Log detail receive content
+		if content != nil {
+			log.Println("RECEIVE Msg:", content.IsMessage, " OP:", content.IsOperation, " type:", content.ContentType, " from:", content.From, "to:", content.To, " ID:", content.ID)
+		}
+		// user add friend
+		if content != nil && content.IsOperation && content.OpType == linebot.OpTypeAddedAsFriend {
+			out := fmt.Sprintf("Bot แปลงพิกัด Eggyo\nวิธีใช้\nเพียงแค่กดแชร์ Location ที่ต้องการ ระบบจะทำการแปลง Location เป็นพิกัดระบบต่างๆ และหาความสูงจากระดับน้ำทะเลให้\n\nหรือจะพูดคุยกับ bot ก็ได้\nกด #help เพื่อดูวิธีใช้อื่นๆ \nติดต่อผู้พัฒนา LINE ID : eggyo")
+			//result.RawContent.Params[0] is who send your bot friend added operation, otherwise you cannot get in content or operation content.
+			_, err = bot.SendText([]string{content.From}, out)
+			if logNof == "open" {
+				bot.SendText([]string{eggyoID}, "bot has a new friend :"+content.From)
+			}
+
+			addNewUser(content.From)
+
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
+		if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
+
+			text, err := content.TextContent()
+			if logNof == "open" {
+				bot.SendText([]string{eggyoID}, "bot get msg:"+text.Text+"\nfrom :"+content.From)
+			}
+			// reply message
+			var processedText = messageCheck(text.Text)
+			_, err = bot.SendText([]string{content.From}, processedText)
+
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		if content != nil && content.ContentType == linebot.ContentTypeLocation {
+			_, err = bot.SendText([]string{content.From}, "ระบบกำลังประมวลผล...")
+
+			loc, err := content.LocationContent()
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+			// add eggyo geo test//
+			resp, err := http.Get("http://eggyo-geo-node.herokuapp.com/geo/" + FloatToString(loc.Latitude) + "," + FloatToString(loc.Longitude))
+=======
+			// add eggyo geo test
+			resp, err := http.Get("http://eggyo-geo-node.herokuapp.com/geo/" + FloatToString(loc.Latitude) + "/" + FloatToString(loc.Longitude))
+>>>>>>> 09a61a620751c49e8b67d3244f5280b4b309e1ae
+=======
+			// add eggyo geo test//
+			resp, err := http.Get("http://eggyo-geo-node.herokuapp.com/geo/" + FloatToString(loc.Latitude) + "," + FloatToString(loc.Longitude))
+>>>>>>> 66cf34abf6b2ed7caa5c018b73c50380be01e401
+			if err != nil {
+				println(err.Error())
+				return
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			log.Println(string(body))
+
+			var elev = callGoogleElev(loc.Latitude, loc.Longitude)
+			geo, err := getGeoLoc([]byte(body))
+			_, err = bot.SendText([]string{content.From}, "LatLong :"+geo.Results.LatLong)
+			_, err = bot.SendText([]string{content.From}, "Utm :"+geo.Results.Utm+"\n\nMgrs :"+geo.Results.Mgrs+"\n\nAltitude :"+elev)
+			if logNof == "open" {
+				bot.SendText([]string{eggyoID}, "bot get loc:"+geo.Results.Mgrs+"\nfrom :"+content.From)
+			}
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 66cf34abf6b2ed7caa5c018b73c50380be01e401
+*/
