0b6185889944d213797c83285a1e06e3636d1db0
eggyo <eggyoyo4859@gmail.com>
2016-10-27 15:33:31 +0700

    v.5.1.2

--- main.go
+++ main.go
@@ -82,14 +82,15 @@ func main() {
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
 					log.Print(message)
 
-				case *linebot.TextMessage:
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
 						log.Print(err)
 					}
 
 				case *linebot.ImageMessage:
+					log.Print(message)
 
 					content, err := bot.GetMessageContent(message.ID).Do()
 					if err != nil {
